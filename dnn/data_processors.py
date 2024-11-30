c13c6159b835a09827ca137727e8a845c43b3cc4
Jeongseop Lim <jseoplim@gmail.com>
2024-11-30 20:18:57 +0900

    refacor: 데이터 처리 파일 분리

---
+++ dnn/data_processors.py
@@ -0,0 +1,30 @@
+from collections.abc import Iterator
+from pathlib import Path
+from typing import NamedTuple
+
+from numpy.typing import NDArray
+
+from dnn.libs import np
+
+
+class Dataset(NamedTuple):
+    x: NDArray[np.float64]  # shape = (B, I)
+    r: NDArray[np.float64]  # shape = (B, 1)
+
+
+def parse_file_to_array(filepath: Path) -> NDArray[np.float64]:
+    """Parse a file with space-separated integers into a numpy array."""
+    return np.loadtxt(filepath, dtype=np.float64)  # type: ignore
+
+
+def generate_random_batches(dataset: Dataset, batch_size: int) -> Iterator[Dataset]:
+    """Generate random batches from the dataset."""
+    x, r = dataset
+
+    if (data_size := x.shape[0]) != r.shape[0]:
+        raise ValueError("The number of features and labels must be the same.")
+
+    indices = np.random.permutation(data_size)
+    for i in range(0, data_size, batch_size):
+        batch_indices = indices[i : i + batch_size]
+        yield Dataset(x[batch_indices], r[batch_indices])
--- dnn/utils.py
+++ dnn/utils.py
@@ -1,6 +1,4 @@
-from collections.abc import Iterator, Sequence
-from pathlib import Path
-from typing import NamedTuple
+from collections.abc import Sequence
 
 from matplotlib import pyplot as plt
 from numpy.typing import NDArray
@@ -25,29 +23,6 @@ def plot_line_graphs(
     plt.xlabel(x_label)
     plt.legend()
     plt.show()
-
-
-def parse_file_to_array(filepath: Path) -> NDArray[np.float64]:
-    """Parse a file with space-separated integers into a numpy array."""
-    return np.loadtxt(filepath, dtype=np.float64)  # type: ignore
-
-
-class Dataset(NamedTuple):
-    x: NDArray[np.float64]  # shape = (B, I)
-    r: NDArray[np.float64]  # shape = (B, 1)
-
-
-def generate_random_batches(dataset: Dataset, batch_size: int) -> Iterator[Dataset]:
-    """Generate random batches from the dataset."""
-    x, r = dataset
-
-    if (data_size := x.shape[0]) != r.shape[0]:
-        raise ValueError("The number of features and labels must be the same.")
-
-    indices = np.random.permutation(data_size)
-    for i in range(0, data_size, batch_size):
-        batch_indices = indices[i : i + batch_size]
-        yield Dataset(x[batch_indices], r[batch_indices])
 
 
 def print_shape(**kwargs: NDArray[np.generic]) -> None:
