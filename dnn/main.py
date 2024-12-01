c864554138c6ebce0dc6e5985b476009c72f798b
Jeongseop Lim <jseoplim@gmail.com>
2024-12-01 17:34:10 +0900

    feat: 실험 세팅. epoch 수정

--- dnn/main.py
+++ dnn/main.py
@@ -43,10 +43,13 @@ base_config = HyperParams(
     max_epoch=250,
 )
 
-lr_configs = [replace(base_config, lr=lr) for lr in lr_list]
+lr_configs = [
+    replace(base_config, lr=lr, max_epoch=max_epoch)
+    for lr, max_epoch in zip(lr_list, [1000, 500, 250, 250])
+]
 batch_size_configs = [
     replace(base_config, batch_size=batch_size, max_epoch=max_epoch)
-    for batch_size, max_epoch in zip(batch_size_list, [10, 250, 125, 500000])
+    for batch_size, max_epoch in zip(batch_size_list, [500, 500, 250, 10000])
 ]
 hidden_nodes_configs = [
     replace(base_config, hidden_nodes=hidden_nodes, max_epoch=max_epoch)
@@ -56,25 +59,25 @@ act_func_configs = [
     replace(base_config, act_func=act_func) for act_func in act_func_list
 ]
 
-print("Base Config")
-export_result(base_config, experiment(base_config, train_data, test_data))
+# print("Base Config")
+# export_result(base_config, experiment(base_config, train_data, test_data))
 
 # print("Change Learning Rate")
 for config in lr_configs:
     # export_result(config, experiment(config, train_data, test_data))
-    pass
+    print()
 
-# print("Change Batch Size")
+print("Change Batch Size")
 for config in batch_size_configs:
-    # export_result(config, experiment(config, train_data, test_data))
-    pass
+    export_result(config, experiment(config, train_data, test_data))
+    print()
 
 # print("Change Hidden Nodes")
 for config in hidden_nodes_configs:
     # export_result(config, experiment(config, train_data, test_data))
-    pass
+    print()
 
 # print("Change Activation Function")
 for config in act_func_configs:
     # export_result(config, experiment(config, train_data, test_data))
-    pass
+    print()
