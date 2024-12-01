0ba1511718f9b69b1195366e08f3542fe41c511f
Jeongseop Lim <jseoplim@gmail.com>
2024-12-01 16:51:12 +0900

    feat: 실험 세팅

--- dnn/main.py
+++ dnn/main.py
@@ -1,4 +1,5 @@
 import os
+from dataclasses import replace
 from pathlib import Path
 
 from dnn.data_processors import Dataset, load_dataset
@@ -11,9 +12,10 @@ if not (test_data_path := os.getenv("TEST_DATA_PATH")):
     raise ValueError("TEST_DATA_PATH environment variable is not set")
 
 train_data: Dataset = load_dataset(Path(train_data_path))
-# train_data = Dataset(train_data.x[:10000], train_data.r[:10000])  # temp
 test_data: Dataset = load_dataset(Path(test_data_path))
 
+"""모델 작동 검사용"""
+# train_data = Dataset(train_data.x[:10000], train_data.r[:10000])  # temp
 hyperparams = HyperParams(
     lr=0.1,
     max_epoch=100,
@@ -21,4 +23,59 @@ hyperparams = HyperParams(
     hidden_nodes=[64, 32],
     act_func=ActFunc.RELU,
 )
-export_result(hyperparams, experiment(hyperparams, train_data, test_data))
+# export_result(hyperparams, experiment(hyperparams, train_data, test_data))
+
+"""Model Selection"""
+# update iter: 약 1,000,000회
+
+# 가변 조건
+lr_list = [1e-4, 1e-3, 1e-2, 1e-1]
+batch_size_list = [1, 32, 64, 128, train_data.x.shape[0]]
+max_epoch_list = [20, 500, 250, 1000000]
+hidden_nodes_list = [[13], [13, 13], [13, 13, 13], [13, 13, 13, 13]]
+act_func_list = [ActFunc.SIGMOID, ActFunc.RELU, ActFunc.LEAKY_RELU, ActFunc.ELU]
+
+# 불변 조건
+base_config = HyperParams(
+    lr=1e-2,
+    batch_size=32,
+    hidden_nodes=[13, 13],
+    act_func=ActFunc.RELU,
+    max_epoch=500,
+)
+
+lr_configs = [replace(base_config, lr=lr) for lr in lr_list]
+batch_size_configs = [
+    replace(base_config, batch_size=batch_size, max_epoch=max_epoch)
+    for batch_size, max_epoch in zip(batch_size_list, max_epoch_list)
+]
+hidden_nodes_configs = [
+    replace(base_config, hidden_nodes=hidden_nodes)
+    for hidden_nodes in hidden_nodes_list
+]
+act_func_configs = [
+    replace(base_config, act_func=act_func) for act_func in act_func_list
+]
+
+print("Base Config")
+export_result(base_config, experiment(base_config, train_data, test_data))
+
+# print("Change Learning Rate")
+for config in lr_configs:
+    export_result(config, experiment(config, train_data, test_data))
+    pass
+
+# print("Change Batch Size")
+for config in batch_size_configs:
+    # export_result(config, experiment(config, train_data, test_data))
+    pass
+
+# print("Change Hidden Nodes")
+for config in hidden_nodes_configs:
+    # export_result(config, experiment(config, train_data, test_data))
+    pass
+
+# print("Change Activation Function")
+for config in act_func_configs:
+    # export_result(config, experiment(config, train_data, test_data))
+    pass
